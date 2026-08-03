6d2bd3ba39e5dd178d17578742a182ed968d9c34
not-a-no <19555822+not-a-no@users.noreply.github.com>
2026-08-03 12:48:37 -0400

    progress indicator added

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main() {
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / double(image_width-1);
             auto g = double(j) / double(image_height-1);
@@ -23,4 +24,6 @@ int main() {
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+
+    std::clog << "\rDone.                           \n";
 }
