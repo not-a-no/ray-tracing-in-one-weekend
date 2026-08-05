f210378e9c8724ebb1341099bf939bc5318f5e7c
not-a-no <19555822+not-a-no@users.noreply.github.com>
2026-08-05 12:49:06 -0400

    the ray class

---
+++ src/ray.h
@@ -0,0 +1,27 @@
+//
+// Created by Nan Ma on 8/5/26.
+//
+
+#ifndef RAY_TRACING_IN_ONE_WEEKEND_RAY_H
+#define RAY_TRACING_IN_ONE_WEEKEND_RAY_H
+
+#include "vec3.h"
+
+class ray {
+public:
+    ray() {}
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+    const point3& origin() const { return orig; }
+    const vec3& direction() const { return dir; }
+
+    point3 at (double t) const {
+        return orig + t * dir;
+    }
+
+private:
+    point3 orig;
+    vec3 dir;
+};
+
+#endif //RAY_TRACING_IN_ONE_WEEKEND_RAY_H
