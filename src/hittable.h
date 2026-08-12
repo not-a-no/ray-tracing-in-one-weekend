309c0e93962458ee04e374cd0a0de8b070233b27
not-a-no <19555822+not-a-no@users.noreply.github.com>
2026-08-12 14:44:48 -0400

    face normal direction defined

---
+++ src/hittable.h
@@ -0,0 +1,33 @@
+//
+// Created by Nan Ma on 8/10/26.
+//
+
+#ifndef RAY_TRACING_IN_ONE_WEEKEND_HITTABLE_H
+#define RAY_TRACING_IN_ONE_WEEKEND_HITTABLE_H
+
+#include "ray.h"
+#include "vec3.h"
+
+class hit_record {
+public:
+    point3 p;
+    vec3 normal;
+    double t = 0.0;
+    bool front_face = false;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter 'outward_normal' is assumed to have unit length.
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class hittable {
+public:
+    virtual ~hittable() = default;
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+};
+
+#endif //RAY_TRACING_IN_ONE_WEEKEND_HITTABLE_H
